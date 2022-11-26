35f4c1ad24fdcee71ba4093fec64c13fec4fdca2
Vladimir <drapejnick@gmail.com>
2022-11-26 11:08:03 +0100

    add correct ex 2

--- Homework_1/002.py
+++ Homework_1/002.py
@@ -4,11 +4,15 @@
 # ⋁ - or
 # ¬ - not
 
-x = int(input('Enter X = '))
-y = int(input('Enter Y = '))
-z = int(input('Enter Z = '))
+List_X = [0, 1]
+List_Y = [0, 1]
+List_Z = [0, 1]
 
-if (x != x or y != y or z != z) == (x != x and y != y and z != z):
-    print(True)
+for i in List_X:
+    for j in List_Y:
+        for k in List_Z:
+            if (not (List_X[i] or List_Y[j] or List_Z[k]) ==
+                    (not List_X[i] and not List_Y[j] and not List_Z[k])):
+                print('True')
 else:
-    print(False)
+    print('False')
